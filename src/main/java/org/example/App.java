e715a2de22b4fcf9e58125116fc5c8e638f6a454
Jaroslav <monatkotata@gmail.com>
2022-07-22 21:16:24 +0300

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,20 @@
+package org.example;
+
+
+import org.example.configuration.MyConfig;
+import org.example.entity.Employee;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
+         Communication communication = context.getBean("communication", Communication.class);
+
+        List<Employee> allEmployees = communication.getAllEmployees();
+        System.out.println(allEmployees);
+    }
+}
